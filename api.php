d212e6b31527029000ed7d2a15d7cb441771260f
Jeffrey Yau <jeffreyyau@outlook.com>
2019-08-29 09:21:17 +0800

    Initial commit
    This commit add the basic template and my new code so far. There's some placeholder images that most likely will be replaced in the future.

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $_SESSION['user'] = $_POST['user'];
+    $_SESSION['title'] = $_POST['title'];
+    $_SESSION['skill'] = $_POST['skill'];
+    $_SESSION['mail'] = $_POST['mail'];
+    $_SESSION['phone'] = $_POST['phone'];
+    $_SESSION['who'] = $_FILES['who'];
+    $_SESSION['layout'] = $_POST['layout'];
+    print_r($_FILES);
+    copy($_FILES['who']['tmp_name'],"upload/".$_FILES['who']['name']);
+    header('location:preview.php');
+?>
