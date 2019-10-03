01fa323af040fa56a27886186bb3216a16df2abd
Jeffrey Yau <jeffreyyau@outlook.com>
2019-10-03 17:04:24 +0800

    style: Change indentation style to two spaces

--- index.php
+++ index.php
@@ -2,51 +2,51 @@
 <html lang="zh-tw">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:100,300,400&display=swap">
-    <link rel="stylesheet" href="css/style.css">
-    <title>vCard Generator</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:100,300,400&display=swap">
+  <link rel="stylesheet" href="css/style.css">
+  <title>vCard Generator</title>
 </head>
 
 <body>
-    <div class="container">
-        <h1>vCard 產生器</h1>
-        <form method="post" action="api.php" enctype="multipart/form-data">
-            <div class="info">
-                <p>
-                    <span class="mr15">名稱</span><input type="text" name="user" value="新海誠">
-                </p>
-                <p>
-                    <span class="mr15">職稱</span><input type="text" name="title" value="前端網頁工程師">
-                </p>
-                <p>
-                    <span class="mr10" style="vertical-align:top">技能</span>
-                    <textarea name="skill" cols=30 rows=10>HTML/CSS網頁概念基礎，JS/JQ前端網頁設計，Vue.js框架設計規劃，PHP/MySQL資料處理，Adobe Ps/Ai平面設計，UI/UX設計</textarea>
-                </p>
-                <p>
-                    <span class="mr5">email</span><input type="text" name="mail" value="makoto2019@gmail.com">
-                </p>
-                <p>
-                    <span class="mr15">電話</span><input type="text" name="phone" value="0900-000-123">
-                </p>
-                <hr>
-                <p>
-                    <span class="mr15">照片</span><input type="file" name="who">
-                </p>
-                <p>
-                    <span class="mr15">版型</span>
-                    <select name="layout">
-                        <option value="rainbow" selected>彩虹</option>
-                        <option value="scratch">刮痕</option>
-                        <option value="wood">木紋</option>
-                    </select>
-                </p>
-            </div>
-            <input type="submit" value="預覽">
-        </form>
-    </div>
+  <div class="container">
+    <h1>vCard 產生器</h1>
+    <form method="post" action="api.php" enctype="multipart/form-data">
+      <div class="info">
+        <p>
+          <span class="mr15">名稱</span><input type="text" name="user" value="新海誠">
+        </p>
+        <p>
+          <span class="mr15">職稱</span><input type="text" name="title" value="前端網頁工程師">
+        </p>
+        <p>
+          <span class="mr10" style="vertical-align:top">技能</span>
+          <textarea name="skill" cols=30 rows=10>HTML/CSS網頁概念基礎，JS/JQ前端網頁設計，Vue.js框架設計規劃，PHP/MySQL資料處理，Adobe Ps/Ai平面設計，UI/UX設計</textarea>
+        </p>
+        <p>
+          <span class="mr5">email</span><input type="text" name="mail" value="makoto2019@gmail.com">
+        </p>
+        <p>
+          <span class="mr15">電話</span><input type="text" name="phone" value="0900-000-123">
+        </p>
+        <hr>
+        <p>
+          <span class="mr15">照片</span><input type="file" name="who">
+        </p>
+        <p>
+          <span class="mr15">版型</span>
+          <select name="layout">
+            <option value="rainbow" selected>彩虹</option>
+            <option value="scratch">刮痕</option>
+            <option value="wood">木紋</option>
+          </select>
+        </p>
+      </div>
+      <input type="submit" value="預覽">
+    </form>
+  </div>
 </body>
 
 </html>
--- preview.php
+++ preview.php
@@ -1,34 +1,35 @@
 <?php
-session_start();
+  session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="zh-tw">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="css/<?= $_SESSION['layout'] ?>.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="css/<?= $_SESSION['layout'] ?>.css">
 </head>
 
 <body>
-    <div class="card-main">
-        <img src="upload/<?= $_SESSION['who']['name'] ?>" class="who">
-        <h2>關於
-            <span class="clr"><?= $_SESSION['user'] ?></span>
-        </h2>
-        <h4 class="clr"><?= $_SESSION['title'] ?></h4>
-        <hr class="hr">
-        <p><?= $_SESSION['skill'] ?></p>
-        <div class="box"><?= $_SESSION['mail'] ?></div>
-        <div class="box"><?= $_SESSION['phone'] ?></div>
-        <button style="margin:50px auto;display:block" onclick="window.history.go(-1)">回頁面</button>
-    </div>
-    <?php
-    session_unset();
-    session_destroy();
-    ?>
+  <div class="card-main">
+    <img src="upload/<?= $_SESSION['who']['name'] ?>" class="who">
+    <h2>關於
+      <span class="clr"><?= $_SESSION['user'] ?></span>
+    </h2>
+    <h4 class="clr"><?= $_SESSION['title'] ?></h4>
+    <hr class="hr">
+    <p><?= $_SESSION['skill'] ?></p>
+    <div class="box"><?= $_SESSION['mail'] ?></div>
+    <div class="box"><?= $_SESSION['phone'] ?></div>
+    <button style="margin:50px auto;display:block" onclick="window.history.go(-1)">回頁面</button>
+  </div>
+  <?php
+  session_unset();
+  session_destroy();
+  ?>
 </body>
 
 </html>
